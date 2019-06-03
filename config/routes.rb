0f965558f602e663c9fa31a73328f3da98906eb0
davidjrada <davidjrada@gmail.com>
2019-06-02 23:39:41 -0400

    configured user controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
   scope '/api' do
     resources :feed_cards
+    post 'user_token' => 'user_token#create'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
