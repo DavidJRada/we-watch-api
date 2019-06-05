500a2195fe6cfa76ef114f48836c6d9a630b58e7
davidjrada <davidjrada@gmail.com>
2019-06-05 14:12:08 -0400

    added user column to feed_cards

--- app/controllers/feed_cards_controller.rb
+++ app/controllers/feed_cards_controller.rb
@@ -1,5 +1,5 @@
 class FeedCardsController < ApplicationController
-  before_action :authenticate_user
+  # before_action :authenticate_user
   before_action :set_feed_card, only: [:show, :update, :destroy]
 
   # GET /feed_cards
